ead6091b35a526294e231d22222325f7eb12e59a
Eli Rosenblum <parubok4@gmail.com>
2020-07-16 09:14:23 +0300

    Updates.

--- src/test/java/org/swingk/winfont/InvalidSystemPropertyTest.java
+++ src/test/java/org/swingk/winfont/InvalidSystemPropertyTest.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test;
 public class InvalidSystemPropertyTest {
     @Test
     void invalidFontDir() {
-        System.setProperty(WinFontFactory.DIR_PROP, "non-existing-dir-3");
+        System.setProperty(WinFontFactory.DIR_PROP, "*non-existing-dir-3*");
         for (WinFont winFont : WinFont.values()) {
-            Assertions.assertThrows(FontUnavailableException.class, () -> WinFontFactory.getFont(winFont));
+            try {
+                WinFontFactory.getFont(winFont);
+                Assertions.fail("Exception was not thrown.");
+            } catch (FontUnavailableException e) {
+                Assertions.assertFalse(e.isFontsDirectoryAvailable());
+                Assertions.assertNotNull(e.getMessage());
+                Assertions.assertNotNull(e.getLocalizedMessage());
+            }
         }
         System.clearProperty(WinFontFactory.DIR_PROP);
     }
